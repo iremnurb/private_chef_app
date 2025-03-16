041bad20882e032d1634686eb3ef091d4d0c2340
iremnurb <ir3mnur21@icloud.com>
2025-03-16 18:29:02 +0300

    recipe mode ve login işlemleri

--- lib/ui/cubit/profil_sayfa_cubit.dart
+++ lib/ui/cubit/profil_sayfa_cubit.dart
@@ -7,3 +7,12 @@ class ProfilSayfaCubit extends Cubit<int> {
     emit(index);
   }
 }
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ProfilSayfaCubit extends Cubit<int> {
+  ProfilSayfaCubit() : super(0);
+
+  void navigateTo(int index) {
+    emit(index);
+  }
+}
