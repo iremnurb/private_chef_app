d88abe9c24bd01d34f59bdd847907d8a68bd5746
iremnurb <ir3mnur21@icloud.com>
2025-03-20 00:03:11 +0300

    Add build.zip with Git LFS

--- lib/ui/cubit/profil_sayfa_cubit.dart
+++ lib/ui/cubit/profil_sayfa_cubit.dart
@@ -1,14 +1,4 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-class ProfilSayfaCubit extends Cubit<int> {
-  ProfilSayfaCubit() : super(0);
-
-  void navigateTo(int index) {
-    emit(index);
-  }
-}
-import 'package:flutter_bloc/flutter_bloc.dart';
-
 class ProfilSayfaCubit extends Cubit<int> {
   ProfilSayfaCubit() : super(0);
 
